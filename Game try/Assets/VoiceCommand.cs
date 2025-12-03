8cb864971a97a2b908542f0e178da118a8f2d9a1
Schuppy <jmsullano404@gmail.com>
2025-12-03 19:23:37 +0800

    Main game

---
+++ Game try/Assets/VoiceCommand.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq; // <EAdd this so we can use .ToArray()
+
+public class VoiceCommand : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    void Start()
+    {
+        // Define cue words and what they do
+        keywords.Add("punch", () => Debug.Log("Punch detected!"));
+        keywords.Add("kick", () => Debug.Log("Kick detected!"));
+        keywords.Add("jump", () => Debug.Log("Jump detected!"));
+
+        // Convert dictionary keys to a string array
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordRecognized;
+        keywordRecognizer.Start();
+
+        Debug.Log("Voice recognizer started. Say: 'punch', 'kick', or 'jump'.");
+    }
+
+    private void OnKeywordRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log($"You said: {args.text}");
+        keywords[args.text].Invoke();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (keywordRecognizer != null && keywordRecognizer.IsRunning)
+        {
+            keywordRecognizer.Stop();
+            keywordRecognizer.Dispose();
+        }
+    }
+}
---
+++ Game try/Assets/VoiceMove2D.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+
+public class VoiceMove2D : MonoBehaviour
+{
+    KeywordRecognizer recognizer;
+    Dictionary<string, System.Action> commands = new Dictionary<string, System.Action>();
+
+    public float speed = 5f;
+    Vector2 direction = Vector2.zero;
+
+    void Start()
+    {
+        // Voice commands list
+        commands.Add("up", () => direction = Vector2.up);
+        commands.Add("down", () => direction = Vector2.down);
+        commands.Add("left", () => direction = Vector2.left);
+        commands.Add("right", () => direction = Vector2.right);
+        commands.Add("stop", () => direction = Vector2.zero);
+
+        // Setup recognizer
+        recognizer = new KeywordRecognizer(commands.Keys.ToArray());
+        recognizer.OnPhraseRecognized += OnCommand;
+        recognizer.Start();
+    }
+
+    void OnCommand(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Heard: " + args.text);
+        commands[args.text].Invoke();
+    }
+
+    void Update()
+    {
+        // Move the player in 2D
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
+
